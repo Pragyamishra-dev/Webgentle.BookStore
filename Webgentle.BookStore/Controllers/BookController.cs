8e7da4c0df99570407a6afbd682dbc79f03ac8f3
Pragyamishra-dev <pm4564993@gmail.com>
2024-03-19 00:48:08 +0530

    19.03.2024

---
+++ Webgentle.BookStore/Controllers/BookController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Webgentle.BookStore.Controllers
+{
+    public class BookController : Controller
+    {
+        public string GetAllBooks()
+        {
+            return "All Books";
+        }
+        public string GetBook(int id)
+        {
+            return $"book with id={id}";
+        }
+
+        public string SearchBooks(string bookName,string authorName)
+        {
+            return $"book with name={bookName} & Author ={authorName}";
+        }
+    }
+}
