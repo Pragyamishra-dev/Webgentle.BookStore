ca38f7b548a598d6812aaaa6f008d7acbf11d8e0
Pragyamishra-dev <pm4564993@gmail.com>
2024-03-21 00:20:37 +0530

    21.March.2024

--- Webgentle.BookStore/Controllers/BookController.cs
+++ Webgentle.BookStore/Controllers/BookController.cs
@@ -1,21 +1,30 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+using Webgentle.BookStore.Models;
+using Webgentle.BookStore.Repository;
 
 namespace Webgentle.BookStore.Controllers
 {
     public class BookController : Controller
     {
-        public string GetAllBooks()
-        {
-            return "All Books";
+        private readonly BookRepository _bookRepository = null;
+        public BookController() 
+        { 
+            _bookRepository=new BookRepository();
         }
-        public string GetBook(int id)
+        public List<BookModel> GetAllBooks()
         {
-            return $"book with id={id}";
+            return _bookRepository.GetAllBooks();
+
+        }
+        public BookModel GetBook(int id)
+        {
+            return _bookRepository.GetBookById(id);
         }
 
-        public string SearchBooks(string bookName,string authorName)
+        public List<BookModel> SearchBooks(string bookName,string authorName)
         {
-            return $"book with name={bookName} & Author ={authorName}";
+            return _bookRepository.SearchBook(bookName, authorName);
         }
     }
 }
---
+++ Webgentle.BookStore/Repository/BookRepository.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Webgentle.BookStore.Models;
+
+namespace Webgentle.BookStore.Repository
+{
+    public class BookRepository
+    {
+        public List<BookModel> GetAllBooks()
+        {
+            return DataSource();
+        }
+        
+        public BookModel GetBookById(int Id)
+        {
+            return DataSource().Where(x=>x.Id == Id).FirstOrDefault();
+        }
+        public List<BookModel> SearchBook(string title, string authoename)
+        {
+            return DataSource().Where(x => x.Title.Contains(title) || x.Author.Contains(authoename)).ToList();
+        }
+        private List<BookModel> DataSource()
+        {
+            return new List<BookModel>()
+        {
+            new BookModel(){Id=1,Title="MVC",Author="Pragya"},
+            new BookModel(){Id=2,Title="Dot Net Core",Author="Priyanka"},
+            new BookModel(){Id=3,Title="C#",Author="Archana"},
+            new BookModel(){Id=4,Title="JAVA",Author="Pragya"},
+            new BookModel(){Id=5,Title="Php",Author="Pragya"},
+
+        };
+    }
+}
+
+}
