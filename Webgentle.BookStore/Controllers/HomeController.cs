e78867f579de7db7a21e95f1ef0b2604c692b3f9
Pragyamishra-dev <pm4564993@gmail.com>
2024-03-25 00:37:06 +0530

    25.03.2024

--- Webgentle.BookStore/Controllers/HomeController.cs
+++ Webgentle.BookStore/Controllers/HomeController.cs
@@ -6,6 +6,9 @@ namespace Webgentle.BookStore.Controllers
     {
         public ViewResult Index()
         {
+            //var obj = new { Id = 1, Name = "Pragya" };
+            //return View("~/TempView/PragyaTemp.cshtml");
+            //return View("../../TempView/PragyaTemp");
             return View();
         }
         public ViewResult AboutUs()
@@ -16,5 +19,6 @@ namespace Webgentle.BookStore.Controllers
         {
             return View();
         }
-    }
+    }//913933187867
+    //840221
 }
