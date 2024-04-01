e6b43bba59fca47e35e871179484f4dd43f1ae95
Pragyamishra-dev <pm4564993@gmail.com>
2024-04-01 23:32:15 +0530

    01.04.2024

--- Webgentle.BookStore/Controllers/HomeController.cs
+++ Webgentle.BookStore/Controllers/HomeController.cs
@@ -1,4 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+using Webgentle.BookStore.Models;
 
 namespace Webgentle.BookStore.Controllers
 {
@@ -6,6 +8,13 @@ namespace Webgentle.BookStore.Controllers
     {
         public ViewResult Index()
         {
+            ViewBag.Title = "Pragya Mishra";
+            dynamic data = new ExpandoObject();
+            data.Id = 1;
+            data.Name = "Romi Mishra";
+            ViewBag.Data = data;
+            //ViewBag.Data = new { Id = 1, Name = "Pragya" };
+            ViewBag.Type = new BookModel() { Id = 5, Author = "Romi Mishra" };
             return View();
         }
         public ViewResult AboutUs()
